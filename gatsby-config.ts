698c0d817d12f440116da0c8069430cfb30252a3
Byron Denham <me@byrondenham.xyz>
2024-12-27 15:21:30 +0200

    initial commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,28 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `ATTIK`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss"]
+	siteMetadata: {
+		title: `ATTIK`,
+		siteUrl: `https://attik.byrondenham.xyz`,
+	},
+	// More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+	// If you use VSCode you can also use the GraphQL plugin
+	// Learn more at: https://gatsby.dev/graphql-typegen
+	graphqlTypegen: true,
+	plugins: [
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: { name: `content`, path: `${__dirname}/content/` },
+		},
+		{
+			resolve: `gatsby-transformer-remark`,
+			options: {
+				resolve: `gatsby-remark-katex`,
+				options: { strict: `ignore` },
+			},
+		},
+		"gatsby-plugin-postcss",
+	],
 };
 
 export default config;
